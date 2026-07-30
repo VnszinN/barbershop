2076a319aafcd42a1e275cb11a633a76ac75a63c
VnszinN <vinicius.saidy@gmail.com>
2026-07-30 16:44:27 -0300

    chore: adicionar tema escuro do shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+// import { Geist } from "next/font/google"
+// import { cn } from "@/app/_lib/utils"
 import "./globals.css"
-import { Geist } from "next/font/google"
-import { cn } from "@/app/_lib/utils"
 
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+// const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
